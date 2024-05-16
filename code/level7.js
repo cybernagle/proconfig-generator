154d939d8afaf4db5b54e83bb1f36f70977b25e1
Nagle Zhang <1247555725@qq.com>
2024-05-16 21:31:33 +0800

    feat: level 7

--- code/level7.js
+++ code/level7.js
@@ -1,3 +1,22 @@
 function main(params) {
-    return false
+    const {user, system} = params;
+    return hasNumberExceedingXByMoreThan1000(system, user)
 }
+
+function isInteger(str) {
+    return /^-?\d+$/.test(str);
+}
+
+function hasNumberExceedingXByMoreThan1000(str, xStr) {
+    if (!isInteger(xStr)) {
+        return false;
+    }
+    const x = parseInt(xStr, 10);
+    const matches = str.match(/\d+/g) || [];
+    return matches.some(numStr => {
+        const num = parseInt(numStr, 10);
+        return num > x && num - x > 1000;
+    });
+}
+
+console.log(main({user: "1", system: "1+1=900"}))
